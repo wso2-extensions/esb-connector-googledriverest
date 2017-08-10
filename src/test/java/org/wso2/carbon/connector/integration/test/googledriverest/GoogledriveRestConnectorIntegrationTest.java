6a56f0f3d606216f9e007e1435b18b06b274017f
Kanapriya <you@example.com>
2017-08-10 18:18:22 +0530

    Add version in IntegrationTest file

--- src/test/java/org/wso2/carbon/connector/integration/test/googledriverest/GoogledriveRestConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/googledriverest/GoogledriveRestConnectorIntegrationTest.java
@@ -46,7 +46,7 @@ public class GoogledriveRestConnectorIntegrationTest extends ConnectorIntegratio
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("googledriverest-connector-1.0.2-SNAPSHOT");
+        init("googledriverest-connector-1.0.3");
         esbRequestHeadersMap = new HashMap<String, String>();
         apiRequestHeadersMap = new HashMap<String, String>();
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
